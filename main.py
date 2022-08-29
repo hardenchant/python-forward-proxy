d4a6b2e97320eb2311df54246441efae98eed4b8
Gleb Lysov <17965301+hardenchant@users.noreply.github.com>
2022-08-29 14:12:15 +0300

    debug logs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 import os
 
 import httpx
@@ -7,9 +8,14 @@ from fastapi import FastAPI, Request, Response
 PROXY = os.getenv('HTTP_PROXY', None)
 PROXY_URL = os.getenv('PROXY_URL', 'https://api.telegram.org')
 TIMEOUT_SECONDS = int(os.getenv('TIMEOUT_SECONDS', 5))
+DEBUG = os.getenv('DEBUG', None)
 
 if PROXY_URL and PROXY_URL[-1] == '/':
     PROXY_URL = PROXY_URL[:-1]
+
+logger = logging.getLogger('uvicorn.debug')
+if DEBUG:
+    logger.setLevel(logging.DEBUG)
 
 app = FastAPI()
 
@@ -26,7 +32,20 @@ async def proxy_request_to_proxy(request: Request):
 
     content = await request.body()
 
+    logger.debug(PROXY)
+    logger.debug(TIMEOUT_SECONDS)
+    logger.debug(PROXY_URL)
+    logger.debug(full_path)
+    logger.debug(headers)
+    logger.debug(method)
+    logger.debug(content)
+
     async with httpx.AsyncClient(proxies=PROXY, headers=headers, timeout=TIMEOUT_SECONDS) as client:
         result = await asyncio.gather(client.request(method, f'{PROXY_URL}/{full_path}', content=content))
         response = result[0]
+
+    logger.debug(response.content)
+    logger.debug(response.status_code)
+    logger.debug(response.headers)
+
     return Response(response.content, response.status_code, response.headers)
